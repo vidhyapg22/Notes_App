d0903a7cf1e02f4d86cba4b4952943a1e6b54675
vidhyapg22 <gadewarvidhya2202@gmail.com>
2023-10-18 19:05:22 +0530

    Version_1

---
+++ app/src/main/java/com/example/notesapp/ui/Fragment/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.notesapp.ui.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.notesapp.ViewModel.NotesViewModel
+import com.example.notesapp.R
+import com.example.notesapp.databinding.FragmentHomeBinding
+import com.example.notesapp.ui.Adapter.NotesAdapter
+
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+    val viewModel: NotesViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+
+        viewModel.getNotes().observe(viewLifecycleOwner,{notesList ->
+//            for(i in notesList){
+//               Log.e("@@@@","onCreateView: $i")
+//            }
+            binding.revAllNotes.layoutManager = GridLayoutManager(requireContext(),2)
+            binding.revAllNotes.adapter= NotesAdapter(requireContext(),notesList)
+
+        })
+
+        binding.btnAddNotes.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_createNoteFragment)
+        }
+        return binding.root
+    }
+
+
+}
